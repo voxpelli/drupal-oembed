5e73ccd7e65b3cb21dee99c28f145166be8eb549
Pelle Wessman <pelle@kodfabrik.se>
2009-10-01 17:33:56 +0200

    Code review of the oembed module

--- theme/oembed-rich.tpl.php
+++ theme/oembed-rich.tpl.php
@@ -1,3 +1,10 @@
+<?php
+// $Id$
+/**
+ * @file
+ * Default template file for oembed data of the rich type
+ */
+?>
 <div class="oembed">
   <?php if (!empty($title)): ?>
     <?php print l($title, $original_url, array('absolute' => TRUE, 'attributes' => array('class' => 'oembed-title'))); ?>
